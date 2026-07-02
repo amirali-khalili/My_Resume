b3abd9d79553ce78e1cbb73bb0fa6c42b1a62616
Amirali <amiiraliborji1388@gmail.com>
2026-07-02 22:43:58 +0330

    .

--- Frontend/src/Context/ThemeContext.jsx
+++ Frontend/src/Context/ThemeContext.jsx
@@ -5,6 +5,7 @@ const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(true);
+  
 
   useEffect(() => {
     const savedMode = localStorage.getItem("darkMode") === "true";
